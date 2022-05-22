de4367ffb1f2e46499f321ba7e2e0aef204f3faf
Roko <rokokovac98@gmail.com>
2022-05-22 21:35:21 +0200

    Add initial migration

---
+++ src/backend/Wcenzije/Wcenzije.API/Migrations/20220522192740_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Wcenzije.API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImageUrls = table.Column<List<string>>(type: "text[]", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    LikeCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}

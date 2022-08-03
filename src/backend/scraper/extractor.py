bb3b32e98f1c0ecd3167d1207156f5b80bbd031f
Roko <rokokovac98@gmail.com>
2022-08-03 23:03:58 +0200

    Save to file, handle rate limit

--- src/backend/scraper/extractor.py
+++ src/backend/scraper/extractor.py
@@ -4,6 +4,7 @@ from os import listdir
 from os.path import isfile, join
 import re
 from os import path
+import time
 from imgur_python import Imgur
 
 path = '.'
@@ -18,17 +19,37 @@ def main():
     id = input('Imgur Client ID:')
     secret = input('Imgur Client Secret:')
 
+    posts_path = join(path, "posts")
+
+    current = 0
+
     for folder in folders:
-        folder_path = join(path, folder)
-                
+        current = current + 1
+        print(f'Extracting folder {current} of {folders.count}')
+
+        folder_path = join(path, folder)  
         post_info = extract_post_info(folder_path)
+
         content = extract_content(post_info)
         rating = extract_rating(content)
         like_count = extract_like_count(post_info)
         name, lat, lng = extract_location(post_info)
         image_urls = upload_images(folder_path, id, secret)
 
-        print(image_urls)
+        post = {
+            "name": name,
+            "content": content,
+            "rating": rating,
+            "like_count": like_count,
+            "location": f"{lat},{lng}",
+            "imageUrls": image_urls
+        }
+
+        post_path = join(posts_path, f"{folder}.json")
+
+        with open(post_path, "w") as outfile:
+            json.dump(post, outfile) 
+    
 
 def extract_content(post_info):
     return post_info['node']['edge_media_to_caption']['edges'][0]['node']['text']
@@ -62,6 +83,8 @@ def extract_post_info(folder_path):
         return json.loads(stri)
 
 def upload_images(folder_path, id, secret):
+    print(f'Uploading images from {folder_path}...')
+
     files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
     image_paths = [join(folder_path, f) for f in files if f.endswith('.jpg')]
     imgur_client = Imgur({'client_id': id, 'access_token': secret})
@@ -69,8 +92,19 @@ def upload_images(folder_path, id, secret):
 
     for path in image_paths:
         response = imgur_client.image_upload(path, 'Untitled', 'Wcenzije test')
+
+        while response['status'] == 429:
+            rate_limit_header = response['response'].headers['X-Ratelimit-Userreset']
+            cooldown = int(rate_limit_header) + 5
+            print(f'Rate limited. Waiting for {cooldown} seconds.')
+            time.sleep(cooldown)
+            response = imgur_client.image_upload(path, 'Untitled', 'Wcenzije test')
+
         image_url = response['response']['data']['link']
         image_urls.append(image_url)
+
+    print('Sleeping for 5 seconds...')
+    time.sleep(5)
 
     return image_urls
 

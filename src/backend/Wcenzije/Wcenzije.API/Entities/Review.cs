c653e2ee1320770deb8e46336921fdfeb5e6fb89
Roko <rokokovac98@gmail.com>
2022-09-17 15:18:37 +0200

    Add Dates to Reviews

--- src/backend/Wcenzije/Wcenzije.API/Entities/Review.cs
+++ src/backend/Wcenzije/Wcenzije.API/Entities/Review.cs
@@ -5,6 +5,8 @@ namespace Wcenzije.API.Entities
     public class Review
     {
         public long Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
         public List<string> ImageUrls { get; set; }
         public string Content { get; set; }
         public string Name { get; set; }
---
+++ src/backend/Wcenzije/Wcenzije.API/Migrations/20220917124359_Review_Dates.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wcenzije.API.Migrations
+{
+    public partial class Review_Dates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Reviews",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Reviews",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "DateUpdated",
+                table: "Reviews");
+        }
+    }
+}

c5547a6bcd0dfa584d540aeaaeb732fda7a27319
Roko <rokokovac98@gmail.com>
2024-02-13 00:19:44 +0100

    Update connection string fetching

--- src/backend/Wcenzije/Wcenzije.API/Setup.cs
+++ src/backend/Wcenzije/Wcenzije.API/Setup.cs
@@ -8,121 +8,98 @@ using Wcenzije.API.Data;
 using Wcenzije.API.Entities;
 
 
-namespace Wcenzije.API
+namespace Wcenzije.API;
+
+public static class Setup
 {
-    public static class Setup
+    public static IServiceCollection SetupAndAddDbContext(this IServiceCollection collection, ConfigurationManager configuration)
     {
-        public static IServiceCollection SetupAndAddDbContext(this IServiceCollection collection, ConfigurationManager configuration)
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+        if (string.IsNullOrEmpty(connectionString))
         {
-            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var connectionString = env is "Development"
-                ? configuration.GetConnectionString("DefaultConnection")
-                : GetHerokuDbConnectionString();
-
-            collection.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
-
-            return collection;
+            throw new Exception("Connection string is null or empty! Make sure ENV ConnectionString is set up.");
         }
-        public static IServiceCollection AddCorsForAnyOrigin(this IServiceCollection services, string policy)
+
+        collection.AddDbContext<AppDbContext>(opt => opt.UseNpgsql(connectionString));
+
+        return collection;
+    }
+    public static IServiceCollection AddCorsForAnyOrigin(this IServiceCollection services, string policy)
+    {
+        services.AddCors(options =>
         {
-            services.AddCors(options =>
+            options.AddPolicy(policy, builder =>
             {
-                options.AddPolicy(policy, builder =>
-                {
-                    builder
-                        .AllowAnyOrigin() 
-                        .WithMethods("GET", "POST")
-                        .AllowAnyHeader();
-                });
+                builder
+                    .AllowAnyOrigin() 
+                    .WithMethods("GET", "POST")
+                    .AllowAnyHeader();
             });
+        });
 
-            return services;
-        }
+        return services;
+    }
 
-        public static WebApplicationBuilder AddAndConfigureAuthentication(this WebApplicationBuilder builder)
+    public static WebApplicationBuilder AddAndConfigureAuthentication(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+        builder.Services.AddAuthentication(options =>
         {
-            builder.Services
-                .AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
 
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-
-            }).AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
-                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-                };
-            });
-
-            builder.Services.Configure<IdentityOptions>(options =>
-            {
-                options.Password.RequireDigit = false;
-                options.Password.RequiredLength = 8;
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequireLowercase = false;
-            });
-
-            return builder;
-        }
-
-        public static WebApplicationBuilder AddAndConfigureAuthorization(this WebApplicationBuilder builder)
+        }).AddJwtBearer(options =>
         {
-            builder.Services.AddAuthorization(options =>
+            options.SaveToken = true;
+            options.RequireHttpsMetadata = false;
+            options.TokenValidationParameters = new TokenValidationParameters()
             {
-                options.AddPolicy("Admin", policy => policy.RequireClaim("Role", "Admin"));
-            });
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+            };
+        });
 
-            return builder;
-        }
-
-        private static string GetHerokuDbConnectionString()
+        builder.Services.Configure<IdentityOptions>(options =>
         {
-            var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 8;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        });
 
-            if (connUrl == null)
-            {
-                throw new Exception("Environment variable DATABASE_URL not found.");
-            }
+        return builder;
+    }
 
-            connUrl = connUrl.Replace("postgres://", string.Empty);
-            var pgUserPass = connUrl.Split("@")[0];
-            var pgHostPortDb = connUrl.Split("@")[1];
-            var pgHostPort = pgHostPortDb.Split("/")[0];
-            var pgDb = pgHostPortDb.Split("/")[1];
-            var pgUser = pgUserPass.Split(":")[0];
-            var pgPass = pgUserPass.Split(":")[1];
-            var pgHost = pgHostPort.Split(":")[0];
-            var pgPort = pgHostPort.Split(":")[1];
-            var connectionString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
-
-            return connectionString;
-        }
-
-        public static WebApplication MigrateDatabase(this WebApplication app)
+    public static WebApplicationBuilder AddAndConfigureAuthorization(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthorization(options =>
         {
-            var serviceScopeFactory = app.Services.GetService(typeof(IServiceScopeFactory)) as IServiceScopeFactory;
-            using var scope = serviceScopeFactory?.CreateScope();
-            var dbContext = scope?.ServiceProvider.GetRequiredService<AppDbContext>();
-            if (dbContext is null)
-            {
-                Console.WriteLine("WARNING: dbContext is null! Cannot migrate database.");
-            }
-            dbContext?.Database.Migrate();
+            options.AddPolicy("Admin", policy => policy.RequireClaim("Role", "Admin"));
+        });
 
-            return app;
+        return builder;
+    }
+
+    public static WebApplication MigrateDatabase(this WebApplication app)
+    {
+        var serviceScopeFactory = app.Services.GetService(typeof(IServiceScopeFactory)) as IServiceScopeFactory;
+        using var scope = serviceScopeFactory?.CreateScope();
+        var dbContext = scope?.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (dbContext is null)
+        {
+            Console.WriteLine("WARNING: dbContext is null! Cannot migrate database.");
         }
+        dbContext?.Database.Migrate();
+
+        return app;
     }
 }

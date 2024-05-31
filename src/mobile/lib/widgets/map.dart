20c164d67ac8c874c3aa0971df53ef7e769a2c15
Roko <rokokovac98@gmail.com>
2024-06-01 00:37:42 +0200

    Decrease map zoom #47
    Closes #47

--- src/mobile/lib/widgets/map.dart
+++ src/mobile/lib/widgets/map.dart
@@ -44,7 +44,7 @@ class _MapState extends State<Map> {
   Widget build(BuildContext context) {
     final _initialCameraPosition = CameraPosition(
       target: LatLng(widget.position.latitude, widget.position.longitude),
-      zoom: 18,
+      zoom: 15,
     );
 
     return Scaffold(

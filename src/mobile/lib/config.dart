1ef25ba31f07646953dc1865c5279426b6818694
Roko <rokokovac98@gmail.com>
2024-06-01 14:38:55 +0200

    Add prod/dev config

--- src/mobile/lib/config.dart
+++ src/mobile/lib/config.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Config {
-  String get apiRoot => dotenv.env['API_ROOT']!;
+  bool get isProduction => const bool.fromEnvironment('dart.vm.product');
+  String get apiRoot =>
+      isProduction ? dotenv.env['API_ROOT']! : dotenv.env['API_ROOT_DEV']!;
 }

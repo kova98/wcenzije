bf441f83dbbcb9e3129229c713895e55c87b3dab
Roko <rokokovac98@gmail.com>
2024-06-02 16:40:56 +0200

    Fix styling

--- src/web/wcenzije2/src/components/Hero.tsx
+++ src/web/wcenzije2/src/components/Hero.tsx
@@ -30,14 +30,14 @@ export default function Hero() {
       className={`flex min-h-screen items-center justify-center bg-[#2196F3] text-white ${gotham.className}`}
     >
       <div className="flex flex-col items-center justify-center gap-10 text-center md:flex-row md:text-left">
-        <div className="flex max-w-md flex-col items-center px-5 md:items-start md:px-0">
-          <h1 className="pt-6 text-6xl font-extrabold md:pt-0">
-            {getRandomMessage()}
+        <div className="flex max-w-lg flex-col items-center px-5 md:items-start md:px-0">
+          <h1 className="pt-20 text-6xl font-extrabold md:pt-0">
+            Papir je pregrub za tvoju finu guzu?
           </h1>
-          <p className="col-span-4 py-6 text-2xl text-blue-50">
+          <p className="px-10 py-12 text-2xl text-blue-50 md:px-0">
             Reci to javno u WCenzije aplikaciji!
           </p>
-          <div className="flex gap-6 pt-4">
+          <div className="flex gap-6">
             <GooglePlayBadge />
             <AppStoreBadge onClick={null} />
           </div>

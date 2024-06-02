be8166021ff5e56c73583e8dacc926c7ca2c8d24
Roko <rokokovac98@gmail.com>
2024-06-02 16:17:24 +0200

    Fix styling

--- src/web/wcenzije2/src/components/Hero.tsx
+++ src/web/wcenzije2/src/components/Hero.tsx
@@ -1,31 +1,51 @@
-import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import deviceArt from '../../public/device-art.png';
 import GooglePlayBadge from '@/components/GooglePlayBadge';
 import AppStoreBadge from '@/components/AppStoreBadge';
+import localFont from 'next/font/local';
+const gotham = localFont({ src: './gothamrnd_bold.otf' });
+
+const messages = [
+  'Osjećaš se stisnuto dok stišćeš?',
+  '(ni)jedan listić papira?',
+  'Papir je pregrub za tvoju finu guzu?',
+  'Rola je okrenuta na krivu stranu?',
+  'Daska je prehladna? Pretopla?',
+  'Vidljivi tragovi kočenja u školjci?',
+  'Više curi iz pisoara nego iz tebe?',
+  'Nit’ smrdi, nit’ miriše?',
+  'Četka izgleda kao da je odribala svoje?',
+  'Sapuna nema ni u tragovima?',
+  'Želiš ostaviti recenziju za anale?',
+];
+
+const getRandomMessage = () => {
+  var index = Math.floor(Math.random() * messages.length);
+  return messages[index];
+};
 
 export default function Hero() {
   return (
-    <div className="font-gotham flex min-h-screen items-center justify-center bg-[#2196F3] text-white">
+    <div
+      className={`flex min-h-screen items-center justify-center bg-[#2196F3] text-white ${gotham.className}`}
+    >
       <div className="flex flex-col items-center justify-center gap-10 text-center md:flex-row md:text-left">
-        <div className="flex max-w-md flex-col items-center md:items-start">
-          <h1 className="text-6xl font-extrabold">
-            Tragovi kočenja u školjci?
+        <div className="flex max-w-md flex-col items-center px-5 md:items-start md:px-0">
+          <h1 className="pt-6 text-6xl font-extrabold md:pt-0">
+            {getRandomMessage()}
           </h1>
           <p className="col-span-4 py-6 text-2xl text-blue-50">
             Reci to javno u WCenzije aplikaciji!
           </p>
-          <div className="mt-4 flex gap-6">
+          <div className="flex gap-6 pt-4">
             <GooglePlayBadge />
             <AppStoreBadge onClick={null} />
           </div>
         </div>
-        <div className="max-w-sm md:w-1/2">
+        <div className="max-w-sm md:w-1/3">
           <Image
             src={deviceArt}
             alt="Picture of the app on a device"
-            width={300}
-            height={300}
             objectFit="contain"
           />
         </div>

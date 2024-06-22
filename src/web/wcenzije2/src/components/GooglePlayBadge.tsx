6a48ac140a7f489422fc415c8aede955cb01e669
Roko <rokokovac98@gmail.com>
2024-06-22 20:28:47 +0200

    Replace img with Image

--- src/web/wcenzije2/src/components/GooglePlayBadge.tsx
+++ src/web/wcenzije2/src/components/GooglePlayBadge.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 
 const GooglePlayBadge = () => {
   return (
     <a href="https://play.google.com/store/apps/details?id=com.kova98.wcenzije&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-      <img
-        style={{ height: 53 }}
+      <Image
+        width={180}
+        height={53}
         alt="Get it on Google Play"
         src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg"
       />

c62e72972e5b1e98a6f7fdeebc411d288906e16b
Roko <rokokovac98@gmail.com>
2024-06-22 20:23:00 +0200

    Add favicons

--- src/web/wcenzije2/src/app/layout.tsx
+++ src/web/wcenzije2/src/app/layout.tsx
@@ -17,6 +17,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>WCenzije</title>
+      </head>
       <body className={inter.className}>
         <main>{children}</main>
         <Toaster />

ed7b264f9659280c6a328400c942e4c4e964eed0
Roko <rokokovac98@gmail.com>
2024-06-26 00:03:20 +0200

    Fix favicon

--- src/web/wcenzije2/src/app/layout.tsx
+++ src/web/wcenzije2/src/app/layout.tsx
@@ -23,13 +23,7 @@ export default function RootLayout({
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/src/app/favicon.ico"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>WCenzije</title>
